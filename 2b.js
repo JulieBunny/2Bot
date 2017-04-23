f49feaf1a1d8a426c2e9bdaa491422500d230321
Julie <julie@checkmaid.moe>
2017-04-23 19:55:50 -0400

    Added !2b, !hype and !nextraid placeholder

--- 2b.js
+++ 2b.js
@@ -11,13 +11,39 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    if (msg.content === '!2b') {
+        msg.channel.send('I am currently in development.' +
+            '\n\n' +
+            'Available commands:' +
+            '\n\n' +
+            '!nextraid' +
+            '!hype')
     }
+    if (message.content === '!nextraid') {
+        msg.channel.send('There is currently no scheduled raid.')
+    }
+    if (message.content === '!hype') {
+        var sb = moment('2017-06-20');
+        var now = moment();
+        var diff = sb-now;
+
+        var days = diff / 1000 / 60 / 60 / 24;
+        var hours = (days - (Math.floor(days))) * 24;
+        var minutes = (hours - (Math.floor(hours))) * 60;
+        var seconds = (minutes - (Math.floor(minutes))) * 60;
+        var daysText = Math.floor(days) + " days ";
+        var hoursText = Math.floor(hours)  + " hours ";
+        var minutesText = Math.floor(minutes) + " minutes ";
+        var secondsText = Math.floor(seconds) + " seconds ";
+
+        var diffText = daysText + hoursText + minutesText + secondsText;
+
+        msg.channel.send('Stormblood releases in ' + diffText);
+    }
+
 });
 
-//declare configs
+//get token
 var token = fs.readFileSync("token").toString();
 
-console.log(token);
-// client.login(token);
+client.login(token);
