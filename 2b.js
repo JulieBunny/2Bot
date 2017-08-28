1b2018dbdf8bfe66e9ffc0bfe2bd2cacdc8ff167
Julie <julie@checkmaid.moe>
2017-08-28 10:26:42 -0400

    Removed unused file executable option Changed syntax to ecmascript
    Added package.json

--- 2b.js
+++ 2b.js
@@ -1,10 +1,8 @@
-#!/usr/bin/env node
-
-const Discord = require("discord.js");
 const fs = require("fs");
-const No2TypeB = require('./2b_commands.js');
-
+const Discord = require("discord.js");
 const client = new Discord.Client();
+
+const No2TypeB = require('./2b_commands.js');
 
 client.on('ready', () => {
     console.log("Successfully logged in.");
@@ -12,13 +10,13 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 
-	var commandResult = No2TypeB.executeCommand(msg.content);
+	let commandResult = No2TypeB.executeCommand(msg.content);
 	if (commandResult.output)
 		msg.channel.send(commandResult.output);
 	
 });
 
 //get token
-var token = fs.readFileSync("token").toString();
+let token = fs.readFileSync("token").toString();
 
 client.login(token);
